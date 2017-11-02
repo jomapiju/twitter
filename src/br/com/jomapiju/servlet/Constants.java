30f45a20e6fb699ac95575866c7d143b91c06e29
jomapiju <jomapiju@gmail.com>
2017-11-02 13:26:36 -0200

    Update Constants.java

--- src/br/com/jomapiju/servlet/Constants.java
+++ src/br/com/jomapiju/servlet/Constants.java
@@ -2,7 +2,7 @@ package br.com.jomapiju.servlet;
 
 public class Constants {
 	
-	public static final String CONSUMER_KEY="6thPqh7B9pWhlqXWHTPg";
-	public static final String CONSUMER_SECRET="7ufeaP0Dhd45ci8xUKSI56CoDxDrcbQuiDgZFoyeU";
+	public static final String CONSUMER_KEY="";
+	public static final String CONSUMER_SECRET="";
 
 }
